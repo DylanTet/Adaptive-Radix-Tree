ef5bcc9d320c46727216e95d765c4bd2504a82b1
Dylan <tettemerd@gmail.com>
2024-07-01 13:06:45 -0700

    Initial commit with Node4 and Node16 done

---
+++ include/art/leafNode.hpp
@@ -0,0 +1,24 @@
+#ifndef ART_LEAF_NODE_HPP
+#define ART_LEAF_NODE_HPP
+
+#include "node.hpp"
+
+namespace art {
+
+template <class T> class art;
+
+template <class T> class LeafNode : public Node<T> {
+public:
+  explicit LeafNode(T value);
+  bool isLeaf() const override;
+
+  T value;
+};
+
+template <class T> LeafNode<T>::LeafNode(T value) : value(value){};
+
+template <class T> bool LeafNode<T>::isLeaf() const { return true; }
+
+} // namespace art
+
+#endif // !ART_LEAF_NODE_HPP
---
+++ include/art/node.hpp
@@ -0,0 +1,40 @@
+#ifndef ART_NODE_HPP
+#define ART_NODE_HPP
+
+#include <_types/_uint16_t.h>
+#include <algorithm>
+namespace art {
+template <class T> class Node {
+public:
+  virtual ~Node() = default;
+
+  Node() = default;
+  Node(const Node<T> &other) = default;
+
+  virtual bool isLeaf() const = 0;
+
+  /**
+   * Determines the number of matching bytes between the node's prefix and the
+   * key.
+   *
+   * Given a node with prefix: "abbbd", a key "abbbccc",
+   * check_prefix returns 4, since byte 4 of the prefix ('d') does not
+   * match byte 4 of the key ('c').
+   *
+   * key:     "abbbccc"
+   * prefix:  "abbbd"
+   *           ^^^^*
+   * index:    01234
+   */
+  int checkPrefix(const char *key, int keyLen) const;
+
+  char *prefix = nullptr;
+  uint16_t prefixLen = 0;
+};
+
+template <class T> int Node<T>::checkPrefix(const char *key, int keyLen) const {
+  return std::mismatch(prefix, prefix + prefixLen, key).second - key;
+}
+} // namespace art
+
+#endif // !ART_NODE_HPP
